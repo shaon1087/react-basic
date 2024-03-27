62c5a6cfe8a8ea955e01e955050a97d99f23492d
shaon1087 <shaonsaha1087@gmail.com>
2024-03-28 00:48:19 +0600

    first commit

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl"> 
+        
+            <title />
+            <image />
+            <view/>
+
+
+         </div>
+    );
+};
+
+export default Card;
---
+++ src/components/card/view.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const view = () => {
+    return (
+        <div>
+            <p>109k</p>
+        </div>
+    );
+};
+
+export default view;
